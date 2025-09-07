351cb42530e7979278b4397902bc43420d3424ae
vcombey <vincent.combey@gmail.com>
2025-09-07 12:42:18 +0200

    debug debug

--- Config/Config.hs
+++ Config/Config.hs
@@ -1,11 +1,24 @@
 module Config where
 
-import IHP.Prelude
+import BasicPrelude (print)
+import Data.Default (def)
 import IHP.Environment
 import IHP.FrameworkConfig
+import IHP.Log qualified as Log
+import IHP.Log.Types
+import IHP.Prelude
 
 config :: ConfigBuilder
 config = do
-    -- See https://ihp.digitallyinduced.com/Guide/config.html
-    -- for what you can do here
-    pure ()
+  liftIO $ putStrLn "hello world"
+
+  let settings =
+        LoggerSettings
+          { level = Log.Info,
+            formatter = Log.withTimeFormatter,
+            destination = Log.Stdout Log.defaultBufSize,
+            timeFormat = "%Y-%m-%d %H:%M:%S"
+          }
+  logger <- liftIO $ Log.newLogger settings
+  option logger
+  pure ()
--- Web/Controller/Static.hs
+++ Web/Controller/Static.hs
@@ -1,6 +1,11 @@
 module Web.Controller.Static where
+
+import BasicPrelude (print)
+import IHP.Log qualified as Log
 import Web.Controller.Prelude
 import Web.View.Static.Welcome
 
 instance Controller StaticController where
-    action WelcomeAction = render WelcomeView
+  action WelcomeAction = do
+    Log.info ("WelcomeAction called" :: Text)
+    render WelcomeView
